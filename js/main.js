edb881e3480f882643f989a3706e30bb910bee3c
johnnyhelloworld <johnny58723826@hotmail.fr>
2023-05-18 15:13:14 +0200

    add contents for the second tab

--- js/main.js
+++ js/main.js
@@ -1,8 +1,10 @@
 const tabItems = document.querySelectorAll(".tab-item");
+const tabContentItems = document.querySelectorAll(".tab-content-item");
 
 // Select tab content
 function selectItem(e) {
   removeBorder();
+  removeShow();
   // Add border to current tab
   this.classList.add("tab-border");
   // Grab content item from DOM
@@ -13,6 +15,10 @@ function selectItem(e) {
 
 function removeBorder() {
   tabItems.forEach(item => item.classList.remove("tab-border"));
+}
+
+function removeShow() {
+  tabContentItems.forEach(item => item.classList.remove("show"));
 }
 
 // Listen for tab click
